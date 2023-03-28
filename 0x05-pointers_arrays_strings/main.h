7db729b9d3e3762f5cdf40884fe3fdaddc5edd61
itsmesadi <itsmesadi@gmail.com>
2023-03-27 18:10:32 -0700

    C projects and other low level programming exercices

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,14 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
